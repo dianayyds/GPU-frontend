1b4c72542dd598ad0bfeff53c02d7e3a70063f50
ycx <20281096@bjtu.edu.cn>
2024-04-15 14:13:45 +0800

    4.15

--- src/views/components/w.vue
+++ src/views/components/w.vue
@@ -8,7 +8,7 @@
 
     <div class="gradient-divider"></div>  
     <h1>负载数据:</h1>
-    <el-table :data="winfo" style="width: 100%">
+    <el-table :data="winfo" style="width: 100%" border stripe>
       <el-table-column prop="cpunum" label="cpu数量" width="180" />
       <el-table-column prop="cpucorenum" label="cpu核心数" width="220" />
       <el-table-column prop="load1min" label="一分钟内负载" width="180" />
@@ -19,12 +19,11 @@
     <div class="gradient-divider"></div>
 
     <h1>用户数据:</h1>
-    <el-table :data="users" style="width: 100%">
+    <el-table :data="users" style="width: 100%" border stripe>
       <el-table-column prop="user" label="用户名" width="100" />
-      <el-table-column prop="tty" label="连接方式" width="100" />
-      <el-table-column prop="idle" label="最后一次执行操作的时间" width="180" />
-      <el-table-column prop="jcpu" label="占用cpu时间" width="220" />
-      <el-table-column prop="what" label="正在执行的命令" width="530" />
+      <el-table-column prop="from" label="来自" width="200" />
+      <el-table-column prop="pcpu" label="当前进程占用时间" width="160" />
+      <el-table-column prop="what" label="正在执行的命令" width="650" />
     </el-table>
 
 
